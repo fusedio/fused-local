f0b0fd60b5f40bf7c7d91cac299c7c730ec2adaf
Gabe Joseph <gjoseph92@gmail.com>
2024-06-14 02:27:54 -0600

    update example

--- example.py
+++ example.py
@@ -1,4 +1,3 @@
-import os
 import numpy as np
 import pystac_client
 import fused_local
@@ -9,36 +8,32 @@ from odc.geo.geobox import GeoBox
 
 fused_local.configure_map(
     title="Sentinel-2 demo",
-    center="ski santa fe, nm",
+    center="ski santa fe",
     zoom=10,
 )
 
 
 @fused_local.tile
 def s2_scene_june(gbox: GeoBox) -> xr.Dataset:
-    try:
-        item = pystac.Item.from_file(
-            "https://earth-search.aws.element84.com/v1/collections/sentinel-2-l2a/items/S2A_13SDV_20240601_0_L2A"
-        )
-        print(f"fetched item {item.id} {gbox} {os.getpid()=}")
-        data = odc.stac.load([item], ["red", "green", "blue"], geobox=gbox)
-        print(f"loaded data for {item.id} {gbox}")
-        # idk why odc.stac doesn't handle nodata / offer an option to mask it
-        data = data.where(data != 0, np.nan)
-        return data
-    except KeyboardInterrupt:
-        print("AAAAHAHGAGHA")
-        raise
+    item = pystac.Item.from_file(
+        "https://earth-search.aws.element84.com/v1/collections/sentinel-2-l2a/items/S2A_13SDV_20240601_0_L2A"
+    )
+    print(f"fetched item {item.id} {gbox}")
+    data = odc.stac.load([item], ["red", "green", "blue"], geobox=gbox)
+    print(f"loaded data for {item.id} {gbox}")
+    # idk why odc.stac doesn't handle nodata / offer an option to mask it
+    data = data.where(data != 0, np.nan)
+    return data
 
 
-# @fused_local.tile
-# def s2_scene_march(gbox: GeoBox) -> xr.Dataset:
-#     item = pystac.Item.from_file(
-#         "https://earth-search.aws.element84.com/v1/collections/sentinel-2-l2a/items/S2B_13SDV_20240301_0_L2A"
-#     )
-#     data = odc.stac.load([item], ["red", "green", "blue"], geobox=gbox)
-#     data = data.where(data != 0, np.nan)
-#     return data
+@fused_local.tile
+def s2_scene_march(gbox: GeoBox) -> xr.Dataset:
+    item = pystac.Item.from_file(
+        "https://earth-search.aws.element84.com/v1/collections/sentinel-2-l2a/items/S2B_13SDV_20240301_0_L2A"
+    )
+    data = odc.stac.load([item], ["red", "green", "blue"], geobox=gbox)
+    data = data.where(data != 0, np.nan)
+    return data
 
 
 # @fused_local.tile
@@ -48,16 +43,22 @@ def s2_scene_june(gbox: GeoBox) -> xr.Dataset:
 #     items = client.search(
 #         collections=["sentinel-2-l2a"],
 #         bbox=tuple(gbox.geographic_extent.boundingbox),
-#         datetime="2022-06-01/2022-08-01",
+#         datetime="2022-03-01/2022-04-01",
 #         query={"eo:cloud_cover": {"lt": 10}},
 #         max_items=20,
-#         # sortby="datetime",
 #     ).item_collection()
 
 #     print(f"{len(items)=}")
 
-#     stack = odc.stac.load(items, groupby="solar_day", geobox=gbox, bands=["red", "green", "blue"])
+#     stack = odc.stac.load(
+#         items,
+#         groupby="solar_day",
+#         geobox=gbox,
+#         bands=["red", "green", "blue"],
+#         chunks={"time": 1},
+#     )
+#     stack = stack.isel(time=slice(4))
 #     stack = stack.where(stack != 0, np.nan)
-#     comp = stack.mean("time")
+#     comp = stack.median("time")
 
-#     return comp
+#     return comp.compute()
